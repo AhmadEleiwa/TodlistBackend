fb2642722942ae150b9a0138961e76b8b5b1c484
ahmadeleiwa <ahmadilawa@gmail.com>
2023-05-17 20:00:54 +0300

    Adding cors

--- server.js
+++ server.js
@@ -1,25 +1,37 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const todoRouter = require('./routes/todo-route')
-const { default: mongoose } = require('mongoose')
-let dotenv = require('dotenv').config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const todoRouter = require("./routes/todo-route");
+const { default: mongoose } = require("mongoose");
+let dotenv = require("dotenv").config();
 
-const app = express()
+const app = express();
 
-app.use(bodyParser())
+app.use(bodyParser());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
 
-app.use('/api', todoRouter)
+  next();
+});
 
-app.use('/', async (req, res) =>{
-    return res.status(404).json({message:"Error page not Found 404"})
-})
+app.use("/api", todoRouter);
 
+app.use("/", async (req, res) => {
+  return res.status(404).json({ message: "Error page not Found 404" });
+});
 
-mongoose.connect(process.env.MONGOPATH).then(()=>{
-    console.log("Connect to Database...")
-    app.listen(5000)
-    console.log("listening in port 5000")
-}).catch((err)=>{
-    console.log(err.message)
-})
+mongoose
+  .connect(process.env.MONGOPATH)
+  .then(() => {
+    console.log("Connect to Database...");
+    app.listen(5000);
+    console.log("listening in port 5000");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
