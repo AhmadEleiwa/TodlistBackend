79736fdf0658eb25afe0b01070f160c053d9941a
ahmadeleiwa <ahmadilawa@gmail.com>
2023-05-17 20:24:50 +0300

    Adding cors

--- server.js
+++ server.js
@@ -6,18 +6,20 @@ let dotenv = require("dotenv").config();
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  
+    next();
+  });
+
 app.use(bodyParser());
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
 
-  next();
-});
 
 app.use("/api", todoRouter);
 
